20f8c1a98a92ad0d81f8f0b9c08df48e9638d6cb
刘琰 <yan.liu@yitu-inc.com>
2020-03-31 16:00:16 +0800

    update

--- src/main/java/com/yliu/algorithm/usually/Backtracking.java
+++ src/main/java/com/yliu/algorithm/usually/Backtracking.java
@@ -13,16 +13,16 @@ public class Backtracking {
      */
 
     /**
-     * 例题-矩阵中的路径
+     * 矩阵中的路径
      * 请设计一个函数，用来判断在一个矩阵中是否存在一条包含某字符串所有字符的路径。路径可以从矩阵中的任意一个格子开始，每一步可以在矩阵中向左，向右，向上，向下移动一个格子。
      * 如果一条路径经过了矩阵中的某一个格子，则该路径不能再进入该格子。 例如 a b c e s f c s a d e e 矩阵中包含一条字符串”bcced”的路径，但是矩阵中不包含"abcb"路径，
-     * 因为字符串的第一个字符b占据了矩阵中的第一行第二个格子之后，路径不能再次进入该格子。
      * 回溯-因为不能重复进入同一个格子，相当于对使用过的格子进行标记。
      */
     public boolean hasPath(char[] matrix, int rows, int cols, char[] str){
         int []flag=new int[matrix.length];
         for(int i=0;i<rows;i++){
             for(int j=0;j<cols;j++){
+                //以每一个点作为起点进行遍历
                 if(helper(matrix,rows,cols,i,j,0,str,flag)){
                     return true;
                 }
@@ -30,7 +30,7 @@ public class Backtracking {
         }
         return false;
     }
-    public boolean helper(char[]matrix,int rows,int cols,int i,int j,int k,char[]str,int[]flag){
+    private boolean helper(char[] matrix, int rows, int cols, int i, int j, int k, char[] str, int[] flag){
         int index=i*cols+j;
         if(i<0||i>=rows||j<0||j>=cols||flag[index]==1||matrix[index]!=str[k]){
             return false;
@@ -38,6 +38,7 @@ public class Backtracking {
         if(k==str.length-1){
             return true;
         }
+        //先将点标记，继续往下递归遍历，如果最后找到路径返回true，否则取消标记，返回false
         flag[index]=1;
         if(helper(matrix,rows,cols,i+1,j,k+1,str,flag)
                 ||helper(matrix,rows,cols,i-1,j,k+1,str,flag)
